10f4086ac4ce5ed41b6ceefe31ac21cf2baceb25
Dilnazamanzholova22052006 <154108941+Dilnazamanzholova22052006@users.noreply.github.com>
2023-12-23 20:24:25 +0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,28 @@
+    import models.Point;
+    import models.Shape;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
+    import java.awt.*;
+    import java.io.File;
+    import java.io.FileNotFoundException;
+    import java.util.Scanner;
 
+    public class MyApplication {
+        public static void main(String[] args) throws FileNotFoundException {
+            File file = new File("src/source.txt");
+
+            Scanner sc = new Scanner(file);
+            Shape shape = new Shape();
+
+            while (sc.hasNext()) {
+                double x = sc.nextDouble();
+                double y = sc.nextDouble();
+
+                Point point = new Point(x, y);
+                shape.addPoint(point);
+            }
+
+            System.out.println(shape.calculatePerimeter());
+            System.out.println(shape.getLongest());
+            System.out.println(shape.getAverageSide());
+        }
     }
-}
